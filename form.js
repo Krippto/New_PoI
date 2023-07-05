6caac4267853211bbb30d0dc9cae9b261d7264cb
Krippto <136462177+Krippto@users.noreply.github.com>
2023-07-05 23:10:21 +0530

    Add files via upload
    deleting users in crud crud and refleccting back in web page

--- form.js
+++ form.js
@@ -5,10 +5,6 @@ form.addEventListener('submit', store)
 function store(e){
     e.preventDefault()
     const li = document.createElement('li')
-    const edtBtn = document.createElement('button')
-    const delBtn = document.createElement('button')
-    edtBtn.innerText = "Edit user"
-    delBtn.innerText = "Delete user"
 
     let name = e.target.name.value  //document.getElementById('name').value
     let mail = e.target.email.value   //document.getElementById('email').value
@@ -21,31 +17,43 @@ function store(e){
     //console.log(name, mail, phone)
     axios.post("https://crudcrud.com/api/360c744e7f504664a4d3c9f6dd902c3d/appointmentData",obj)
     .then((res)=>{
-        console.log(res.data)
+        li.setAttribute('id',`${res.data._id}`)
+        var edtBtn = `<button onclick="deleteUser("${res.data._id}")">Delete User</button>`
+        var delBtn = `<button onclick=editUser("${res.data._id}")>Edit User</button>`
+        li.appendChild(edtBtn)
+        li.appendChild(delBtn)
     })
     .catch((err)=>{
         console.log(err)
     })
     li.innerText = `${name} - ${mail} - ${phone}`
-    li.appendChild(edtBtn)
-    li.appendChild(delBtn)
+    
     document.getElementById('toAdd').appendChild(li)
     //localStorage.setItem(obj.mail,JSON.stringify(obj))
 }
 
+function deleteUser(id){
+    axios.delete(`https://crudcrud.com/api/360c744e7f504664a4d3c9f6dd902c3d/appointmentData/${id}`)
+    .then((res)=>{
+        console.log(res.data)
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+    document.getElementById(id).parentElement.remove()
+}
 window.addEventListener('DOMContentLoaded',()=>{
     axios.get("https://crudcrud.com/api/360c744e7f504664a4d3c9f6dd902c3d/appointmentData")
     .then((res)=>{
         for(let i = 0;i<res.data.length;i++){
-            const li = document.createElement('li')
-            const edtBtn = document.createElement('button')
-            const delBtn = document.createElement('button')
-            edtBtn.innerText = "Edit user"
-            delBtn.innerText = "Delete user"
-            li.innerText = `${res.data[i].name} - ${res.data[i].mail} - ${res.data[i].phone}`
-            li.appendChild(edtBtn)
-            li.appendChild(delBtn)
-            document.getElementById('toAdd').appendChild(li)
+            //const li = document.createElement('li')
+            //var edtBtn = `<button onclick=deleteUser(${res.data[i]._id})>Delete User</button>`
+            //var delBtn = `<button onclick=editUser(${res.data[i]._id})>Edit User</button>`
+            const li = `<li id=${res.data[i]._id}> ${res.data[i].name} - ${res.data[i].mail} - ${res.data[i].phone}<button onclick="deleteUser('${res.data[i]._id}')">Delete User</button><button onclick="editUser('${res.data[i]._id}')">Edit User</button></li>`
+            //li.appendChild(edtBtn)
+            //li.appendChild(delBtn)
+            document.getElementById('toAdd').innerHTML = document.getElementById('toAdd').innerHTML + li
+
         }
     })
     .catch((err)=>{
