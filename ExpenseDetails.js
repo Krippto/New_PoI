b7afe81760fbf13784bc4255327f17cab075c1e0
Krippto <136462177+Krippto@users.noreply.github.com>
2023-07-17 15:49:44 +0530

    Add files via upload
    Added event listeners

--- ExpenseDetails.js
+++ ExpenseDetails.js
@@ -4,7 +4,8 @@ const ExpenseDetails = (props) => {
     return(
         <div className='expense-item__description'>
             <h2>{props.title} - {props.location}</h2>
-            <div className='expense-item__price'>{props.amount}</div>
+            <div className='expense-item__price'>${props.amount}</div>
+            <button className='expense-item__price' onClick={()=>{console.log("Clicked!!!!")}}>Delete</button>
         </div>
     )
 }
