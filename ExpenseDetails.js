122e2fa0ae0026652fc68198fb4eedd3b7781b37
Krippto <136462177+Krippto@users.noreply.github.com>
2023-07-17 16:34:59 +0530

    Add files via upload
    Added useState react hook

--- ExpenseDetails.js
+++ ExpenseDetails.js
@@ -1,11 +1,17 @@
+import { useState } from 'react'
 import './ExpenseItem.css'
 
 const ExpenseDetails = (props) => {
+    const [amount, setAmount] = useState(props.amount)
+    const clickHandler = () =>{
+        setAmount(100)
+    }
+
     return(
         <div className='expense-item__description'>
             <h2>{props.title} - {props.location}</h2>
-            <div className='expense-item__price'>${props.amount}</div>
-            <button className='expense-item__price' onClick={()=>{console.log("Clicked!!!!")}}>Delete</button>
+            <div className='expense-item__price'>${amount}</div>
+            <button className='expense-item__price' onClick={clickHandler}>Change Expense</button>
         </div>
     )
 }
